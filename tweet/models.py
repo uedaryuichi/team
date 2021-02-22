a8eb1e569f985728015b70b74a4dc78064f2359e
uedaryuichi <bounceback.kanata@gmail.com>
2021-02-22 09:43:23 +0900

    2/21

--- tweet/models.py
+++ tweet/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
+
+from user.models import User
 
 
 class Post(models.Model):
     content = models.TextField(null=True)
-    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     date_posted = models.DateTimeField(default=timezone.now)
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
